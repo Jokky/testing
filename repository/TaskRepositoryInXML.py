43db8512f903c194b2f1593fe1527168580728da
dmitryyakushev <dima.yakushev.1999@gmail.com>
2020-12-07 19:58:45 +0300

    Add show all tasks from xml

--- repository/TaskRepositoryInXML.py
+++ repository/TaskRepositoryInXML.py
@@ -1,11 +1,30 @@
-
+import os
+from typing import Union, List
 from entity.Task import Task
 from intrefaces.TaskRepository import TaskRepository
-from xml.dom import minidom
+import xml.etree.cElementTree as ET
+
+XML_FILE = os.path.join(os.environ['HOME'], '/Users/dmitryyakushev/PycharmProjects/tasking/repository/1.xml')
+
 
 class TaskRepositoryXML(TaskRepository):
     def __init__(self):
-        self.myFile = minidom.parse('1.xml')
+        myFiletre = ET.ElementTree(file=XML_FILE)
+        self.root = myFiletre.getroot()
 
     def show_all(self) -> Union[List[Task], str]:
-        items = mydoc.getElementsByTagName('item')
+        tasks = []
+        for elem in self.root:
+            task = {}
+            for child in elem:
+                task[child.tag] = child.text
+
+            tasks.append(Task(
+                id=int(task.get('id')),
+                title=task.get('title'),
+                description=task.get('description'),
+                create_at=task.get('create'),
+                update_at=task.get('update'),
+            ))
+
+        return tasks
